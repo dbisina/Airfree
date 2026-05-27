47547faadb54382681d2d894798ebb3eba868702
Daniel <danbis664@gmail.com>
2026-05-27 18:34:09 +0100

    Update Ricttext

--- components/admin/RichTextEditor.tsx
+++ components/admin/RichTextEditor.tsx
@@ -6,7 +6,7 @@ import Underline from '@tiptap/extension-underline';
 import Link from '@tiptap/extension-link';
 import TextAlign from '@tiptap/extension-text-align';
 import Color from '@tiptap/extension-color';
-import TextStyle from '@tiptap/extension-text-style';
+import { TextStyle } from '@tiptap/extension-text-style';
 import Highlight from '@tiptap/extension-highlight';
 import Placeholder from '@tiptap/extension-placeholder';
 import Image from '@tiptap/extension-image';
